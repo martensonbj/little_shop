03907702fd800429e8224907772863c20cfc3da5
ToniRib <tonimarierib@gmail.com>
2016-01-12 09:28:07 -0700

    Add description and price to items database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112012622) do
+ActiveRecord::Schema.define(version: 20160112162728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160112012622) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "price"
+    t.text     "description"
   end
 
   add_index "items", ["artist_id"], name: "index_items_on_artist_id", using: :btree
