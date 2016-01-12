3cbb2141537cf7f99c71421c29b565a500d8a3aa
martensonbj <martensonbj@gmail.com>
2016-01-12 10:17:08 -0700

    Reroute index page to go through HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :items, only: [:index]
   resources :categories, only: [:show], param: :slug
   resources :users, only: [:index]
 
-  root 'users#index'
+  root 'home#index'
 end
