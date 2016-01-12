ffcd52ea38d8a7a3afac0c48c12303dbc2d1535b
ToniRib <tonimarierib@gmail.com>
2016-01-12 16:11:09 -0700

    Delete item from cart
    User can delete an item from the cart by clicking on the 'Remove'
    button next to the item. A flash message will be displayed that also
    allows the user to go to the show page for the item they just deleted
    in case they want to add the item again. The flash message is colored
    green since deleting items is a success message.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,13 +2,25 @@ class CartItemsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
+
     session[:cart] = @cart.contents
-    flash[:notice] = "You added #{item.title} to your cart."
+    flash[:success] = "You added #{item.title} to your cart."
+
     redirect_to request.referer
   end
 
   def index
     @items_with_quantities = @cart.item_quantities
     @total = @cart.total_price
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    @cart.delete_item(item.id)
+
+    link_to_item = "#{view_context.link_to(item.title, item)}"
+    flash[:success] = "Successfully removed #{link_to_item} from your cart."
+
+    redirect_to cart_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   resources :categories, only: [:show], param: :slug
   resources :users, only: [:index]
-  resources :cart_items, only: [:create]
+  resources :cart_items, only: [:create, :destroy]
 
   get "/cart", to: "cart_items#index"
 
