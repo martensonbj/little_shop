4c066edcbf0c11331e4e7a03b9e2a2731596760f
Taylor Moore <Tmoore2272@gmail.com>
2016-01-14 10:07:43 -0700

    Add model test for user and artist

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:username)
+  should validate_uniqueness_of(:username)
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
 end
