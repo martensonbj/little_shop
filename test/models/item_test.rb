8964058b4c1afd3852d1363c33ac404993605347
Taylor Moore <Tmoore2272@gmail.com>
2016-01-14 11:21:54 -0700

    Add model tests to user, items, artist
    Add validations for all schema requirements for each
    user, items and artist. Also added a test for uniquenss
    for username in the user model test.

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:title)
+  should validate_presence_of(:image_path)
+  should validate_presence_of(:artist_id)
+  should validate_presence_of(:category_id)
+  should validate_presence_of(:price)
+  should validate_presence_of(:description)
 end
