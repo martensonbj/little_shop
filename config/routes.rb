c0a2d0a07c49fbfdd273bf090d9082899c66ed03
martensonbj <martensonbj@gmail.com>
2016-01-13 18:24:48 -0700

    Create show page for individual orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:show], param: :slug
   resources :users, only: [:index, :new, :create]
   resources :cart_items, only: [:create, :destroy, :update]
-  resources :orders, only: [:index, :create]
+  resources :orders, only: [:index, :create, :show]
 
   get "/cart", to: "cart_items#index"
   get "/login", to: "sessions#new"
