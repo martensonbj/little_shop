ecd4a7338747626102bd72fdaf24ecf387c87203
ToniRib <tonimarierib@gmail.com>
2016-01-15 11:56:45 -0700

    Create Artists scope on User and artists index
    Allows a user to browse art listed in an index by artist. Creates
    a 'artists' scope on the User class to be able to grab all users
    who are artists. Also adds a full_name method to the User class which
    is a string of their first and last name with a space inbetween them.
    Adds a test for browsing by artist.

---
+++ test/integration/guest_can_view_items_sorted_by_artist_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class GuestCanViewItemsSortedByArtistTest < ActionDispatch::IntegrationTest
+  test "items are shown under their assigned artist" do
+    artist1 = create(:artist_with_items)
+    artist2 = create(:artist_with_items)
+    user = create(:user)
+
+    visit artists_path
+
+    assert page.has_content?(artist1.full_name)
+    within "\##{artist1.username}" do
+      assert page.has_content?(artist1.items.first.title)
+      assert page.has_content?(artist1.items.last.title)
+      refute page.has_content?(artist2.items.first.title)
+      refute page.has_content?(artist2.items.last.title)
+    end
+
+    assert page.has_content?(artist2.full_name)
+    within "\##{artist2.username}" do
+      assert page.has_content?(artist2.items.first.title)
+      assert page.has_content?(artist2.items.last.title)
+      refute page.has_content?(artist1.items.first.title)
+      refute page.has_content?(artist1.items.last.title)
+    end
+
+    refute page.has_content?(user.full_name)
+  end
+end
