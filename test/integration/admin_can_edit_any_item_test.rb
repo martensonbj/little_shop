a9b66f77e4591b2627e7cbce9c9dc4bea27548e9
Taylor Moore <Tmoore2272@gmail.com>
2016-01-16 13:32:11 -0700

    Add edit button on items view page

--- test/integration/admin_can_edit_any_item_test.rb
+++ test/integration/admin_can_edit_any_item_test.rb
@@ -2,22 +2,23 @@ require 'test_helper'
 
 class AdminCanEditAnyItemTest < ActionDispatch::IntegrationTest
   test "admin edit other peoples items" do
-    admin = User.create(first_name: "Admin",
-                        last_name:  "Admin",
-                        username:   "admin",
-                        password:   "password",
-                        role:       2)
-    artist = create(:artist_with_items)
-    category = create(:category)
+    admin = create(:admin)
+    artist = create(:artist)
+    item = create(:item)
+    artist.items << item
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
-    visit edit_admin_item_path(Item.last)
+    visit admin_items_path
+    click_on "Edit"
+
+    assert_equal edit_admin_item_path(item), current_path
+
     fill_in "Title", with: "Meat"
     fill_in "Image path",
             with: "https://petenelson.com/wp-content/uploads/2011/08/ron-swanson-meat.jpg"
     fill_in "Price", with: "10"
     fill_in "Description", with: "Salad? That's what my food eats"
-    select "#{category.name}", from: "item_category_id"
+    select "#{item.category.name}", from: "item_category_id"
     select "#{artist.first_name}", from: "item_user_id"
     click_on "Complete"
 
