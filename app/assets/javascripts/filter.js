8e846ab35ae6554b544c7c307e13d0669d18b388
ToniRib <tonimarierib@gmail.com>
2016-01-19 11:36:54 -0700

    Allow filtering by category name on categories index

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,16 +1,33 @@
 $(document).ready(function () {
   var $artists = $('.artist');
+  var $categories = $('.category');
 
   $('#artist_filter_name').on('keypress', function () {
-    var currentName = this.value;
+    var currentName = this.value.toLowerCase();
+
     $artists.each(function (index, artist) {
       var $artist = $(artist)
-      var $artist_name = $artist.find('h2').text();
+      var $artist_name = $artist.find('h2').text().toLowerCase();
 
       if ($artist_name.startsWith(currentName)) {
         $artist.show();
       } else {
         $artist.hide();
+      }
+    });
+  });
+
+  $('#category_filter_name').on('keypress', function () {
+    var currentName = this.value.toLowerCase();
+
+    $categories.each(function (index, category) {
+      var $category = $(category)
+      var $category_name = $category.find('h2').text().toLowerCase();
+
+      if ($category_name.startsWith(currentName)) {
+        $category.show();
+      } else {
+        $category.hide();
       }
     });
   });
