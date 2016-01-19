b324a04f5ee859d9fef7e80cee16ed60b8597531
ToniRib <tonimarierib@gmail.com>
2016-01-18 20:01:57 -0700

    Add styling to dashboard for user and admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
   helper_method :current_user
+  helper_method :current_admin?
 
   def set_cart
     @cart = Cart.new(session[:cart])
