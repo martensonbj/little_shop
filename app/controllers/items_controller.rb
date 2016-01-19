b9490818b22aa8c78807264b1f41d96bf70e2882
ToniRib <tonimarierib@gmail.com>
2016-01-18 18:21:54 -0700

    Artist can add an item for themselves

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,41 @@
 class ItemsController < ApplicationController
+  before_action :require_arist, only: [:new, :create]
+
   def index
     @items = Item.all
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = current_user.items.new(item_params)
+
+    if item.save
+      redirect_to item
+    else
+      flash.now[:alert] = "Incomplete form"
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,
+                                 :image_path,
+                                 :price,
+                                 :description,
+                                 :category_id,
+                                 :status)
+  end
+
+  def require_arist
+    render file: "/public/404" unless current_artist?
   end
 end
