e603034b123a0096a9fb510c92b9109edc1dfd14
ToniRib <tonimarierib@gmail.com>
2016-01-19 12:05:40 -0700

    Admin can filter orders by order status

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   var $artists = $('.artist');
   var $categories = $('.category');
+  var $orders = $('.order');
 
   $('#artist_filter_name').on('keypress', function () {
     var currentName = this.value.toLowerCase();
@@ -28,6 +29,21 @@ $(document).ready(function () {
         $category.show();
       } else {
         $category.hide();
+      }
+    });
+  });
+
+  $('#order_status').on('change', function () {
+    var currentStatus = $('#order_status :selected').text()
+
+    $orders.each(function (index, order) {
+      var $order = $(order);
+      var $order_status = $order.find('.order_status').text();
+
+      if ($order_status === currentStatus || currentStatus === 'all') {
+        $order.show();
+      } else {
+        $order.hide();
       }
     });
   });
