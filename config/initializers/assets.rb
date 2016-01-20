00b0d88f8122ff9e64ca94a54c6489df89bdb086
martensonbj <martensonbj@gmail.com>
2016-01-20 15:53:20 -0700

    Make home page colors darker

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( favicon.ico bg1.jpg bg2.jpg bg3.jpg )
+Rails.application.config.assets.precompile += %w( favicon.ico bg1.jpg bg2.jpg bg4 )
