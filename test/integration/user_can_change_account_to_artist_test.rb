8aaa65ab219aff781fd9d901c15b1b8eb4fad8a9
ToniRib <tonimarierib@gmail.com>
2016-01-20 12:51:32 -0700

    Artist cannot change back to default user role

--- test/integration/user_can_change_account_to_artist_test.rb
+++ test/integration/user_can_change_account_to_artist_test.rb
@@ -16,6 +16,7 @@ class UserCanChangeAccountToArtistTest < ActionDispatch::IntegrationTest
     click_on "Edit Info"
 
     assert_equal edit_user_path(user), current_path
+    assert page.has_select? "user_role", options: %w(artist default)
 
     fill_in "City", with: "Gotham"
     select("artist", from: "user_role")
@@ -26,14 +27,24 @@ class UserCanChangeAccountToArtistTest < ActionDispatch::IntegrationTest
     assert page.has_link? "Add New Item", href: new_user_item_path(user)
     assert page.has_link? "View My Items", href: artist_path(user)
     assert page.has_content? "Gotham"
+  end
 
+  test "artist cannot change back to a user" do
+    artist = create(:artist)
+
+    visit login_path
+
+    fill_in "Username", with: artist.username
+    fill_in "Password", with: artist.password
+    within "form" do
+      click_on "Login"
+    end
+
+    visit dashboard_path
     click_on "Edit Info"
-    select("default", from: "user_role")
-    click_on "Update User"
 
-    assert_equal dashboard_path, current_path
-    assert_equal "default", User.last.role
-    refute page.has_link? "Add New Item", href: new_user_item_path(user)
-    refute page.has_link? "View My Items", href: artist_path(user)
+    assert_equal edit_user_path(artist), current_path
+    assert page.has_select? "user_role", options: %w(artist)
+    refute page.has_select? "user_role", options: %w(default)
   end
 end
